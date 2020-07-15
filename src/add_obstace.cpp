598f10c803c55637f1d06666f982888b137678e6
TaoistSu <727446778@qq.com>
2020-07-15 20:41:12 +0800

    initial project version

--- src/add_obstace.cpp
+++ src/add_obstace.cpp
@@ -4,9 +4,10 @@ date: 2020-03-30
 因为雷达扫描的是原型，如果用来避障取固定返回距离，是个扇形避障区
 实际上使用是个矩形避障区域，以下代码完成，扇形到矩形避张区域的转换
 只需要修改参数：
---degreeStart和degreeEnd是设定区的扇形范围
+--degreeStart和degreeEnd是设定左边的扇形范围
+--degreeStartII和degreeEndII是设定右边的扇形范围
 --avoid_distances是需要的车身前方避障距离
---car_width是车身宽度
+--car_width是车身宽度的一半，如果车宽2m，car_width设为2
 */
 
 #include <iostream>
@@ -26,22 +27,27 @@ void scanCallback(const sensor_msgs::LaserScan &scan_aux)
 {
     //参数配置
     const double avoid_distances = 2.0; //需要的车身前方避障距离
-    const double car_width = 2.0;       //车身宽度
+    const double car_width = 1.0;       //车身宽度
     const double degreeStart = 0.0; //需要雷达前方的起始扇形范围
     const double degreeEnd = 90.0; //需要雷达前方的结束扇形范围
 
+    const double degreeStartII = 270.0; //需要雷达前方的起始扇形范围
+    const double degreeEndII = 360.0; //需要雷达前方的结束扇形范围
     //初始化
     const double PI = 3.1415926535;
     double preDegree4range = 0.0;
     double startI = 0.0;
     double endI= 0.0;
-    double theta = atan(car_width / avoid_distances);
+    double startII = 0.0;
+    double endII= 0.0;
+    double theta = atan(car_width / avoid_distances);//弧度制的theta
     sensor_msgs::LaserScan scan = scan_aux;
     preDegree4range = scan.ranges.size()/360.0; //1度需要几个range,不能写360,会取整
     startI = int(degreeStart * preDegree4range);//开始角度的数组下标
     endI  = int(degreeEnd * preDegree4range);//结束角度的数组下标
-
-    //开始计算
+    startII = int(degreeStartII * preDegree4range);//开始角度的数组下标
+    endII  = int(degreeEndII * preDegree4range);//结束角度的数组下标
+    //左边的
     for(int i = startI;i < endI; i++)
     {
 	if(i<int((theta*180/PI)*preDegree4range))
@@ -54,6 +60,22 @@ void scanCallback(const sensor_msgs::LaserScan &scan_aux)
 	}
    	std::cout<<"雷达只扫描车身前方的角度:"<<theta*180/PI<<"度"<<std::endl;
     }
+
+    //右边的，这是假定单线雷达放在中间，如果雷达放在车右顶角就把右边的注释掉
+    for(int i = startII;i < endII; i++)
+    {
+	if(i<int((360-theta*180/PI)*preDegree4range))
+	{
+		//生成一条竖线的障碍,(i/degree)是当前角度，横线只覆盖avoid_distances的距离
+       		scan.ranges[i] = car_width/cos((i-startII)/preDegree4range*PI/180);
+	}else{
+		//生成一条横线的障碍,(i/degree)是当前角度，横线只覆盖车身宽度前方
+       		scan.ranges[i] = fabs(avoid_distances/cos(((scan.ranges.size()-i)/preDegree4range)*PI/180));
+	}
+   	//ROS_INFO("--------%d",scan.ranges.size());
+	
+    }
+
     //发布主题scan1
     pub.publish(scan);
 }
